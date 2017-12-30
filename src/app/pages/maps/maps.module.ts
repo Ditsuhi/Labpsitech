6325c7fdea126602126556d992978b514c12aebb
Ditsuhi <al366186@uji.es>
2017-12-30 11:23:43 +0100

    markerCluster

--- src/app/pages/maps/maps.module.ts
+++ src/app/pages/maps/maps.module.ts
@@ -5,6 +5,8 @@ import { AngularEchartsModule } from 'ngx-echarts';
 import { ThemeModule } from '../../@theme/theme.module';
 import { MapsRoutingModule, routedComponents, routedProviders } from './maps-routing.module';
 import { MyDateRangePickerModule } from 'mydaterangepicker';
+import { LeafletMarkerClusterModule } from '@asymmetrik/ngx-leaflet-markercluster';
+import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -14,9 +16,11 @@ import { MyDateRangePickerModule } from 'mydaterangepicker';
     MapsRoutingModule,
     AngularEchartsModule,
     MyDateRangePickerModule,
+    LeafletMarkerClusterModule
   ],
   exports: [],
   declarations: [
+    LeafletDirective,
     ...routedComponents,
   ],
   providers: [
